ef3139f2e53324850e245a27fdec8b0aea49c1c7
daw2-LopezCarod22 <91276534+daw2-LopezCarod22@users.noreply.github.com>
2021-10-01 20:20:56 +0200

    Add files via upload

---
+++ N2P1JonathanComments.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>N2P1</title>
+</head>
+<body>
+	<p>Create a list of the 10 most important concepts about what you’ve learned in this document.</p>
+	<ul>
+		<li>He aprendido a crear sesiones en PHP con Session</li>
+		<li>He aprendido a usar el elemento date</li>
+		<li>He aprendido a implementar cookies en mi codigo php</li>
+		<li>He aprendido a implementar un controll null</li>
+		<li>He aprendido a utilizar el get para coger datos de un php a otro</li>
+		<li>He aprendido a usar el urlencode</li>
+		<li>He aprendido a incorporar script php en un documento html</li>
+		<li>He aprendido a usar formularios junto a php</li>
+		<li>He aprendido a usar links con enlaces php</li>
+		<li>He aprendido a usar inputs</li>
+	</ul>
+	<p>Qualifies the document (0-10), and the teacher's explanation (0-10).<br></p>
+	<p>Document: 5<br></p>
+	<p>Teacher explanation: 6<br></p>
+	<p>As a student, put a mark to yourself.<br></p>
+	<p>Myself mark: 7<br></p>
+	<p>Explain an improvement.<br></p>
+	<p>Mejoraria un poco mas hacer ejemplos practicos antes de enviar la practica, porque he tenido algo de lio haciendola y no me quedaba claro en que parte de la guia podia ayudarme en lo que estaba atascado.</p>
+
+</body>
+</html>
